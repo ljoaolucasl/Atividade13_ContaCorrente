d57a763e24e76d724a8f138b428cb19b6ed74d96
João <Joo@JOO>
2023-04-06 18:29:55 -0300

    Add project files.

---
+++ Atividade13_ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade13_ContaCorrente.ConsoleApp
+{
+    /*  Uma conta corrente possui um número,------------------------
+
+     *  um saldo,---------------------------------------------------
+
+     *  um status que informa se ela é especial ou não,-------------
+
+     *  um limite---------------------------------------------------
+
+     *  visualização de saldo,--------------------------------------
+
+     *  visualização de extrato-------------------------------------
+
+     *  Não precisa implementar a interação com usuário.------------
+
+     */
+
+    /* Uma movimentação possui um valor e uma informação se ela é uma movimentação de crédito ou débito.-------------
+
+     *  Cada conta terá operações de saques,-------------------------------------------------------------------------
+
+     * depósitos,----------------------------------------------------------------------------------------------------
+
+     *  e transferência entre contas.--------------------------------------------------------------------------------
+
+     *  fazer saques desde que o valor não exceda o limite de saque que é o limite + saldo.--------------------------
+     
+    */
+
+
+
+    public class ContaCorrente
+    {
+        private int numeroCliente;
+        private int saldoCliente;
+        private int limiteCliente;
+        private int limiteTotalCliente;
+        private bool especialCliente;
+
+        public ContaCorrente(int numero, int saldo, int limite, bool especial)
+        {
+            numeroCliente = numero;
+            saldoCliente = saldo;
+            limiteCliente = limite;
+            limiteTotalCliente = limite;
+            especialCliente = especial;
+        }
+
+        public string VisualizarSaldo()
+        {
+            return $"Cliente({numeroCliente}) seu saldo é de: R${saldoCliente}";
+        }
+
+        public string VisualizarLimite()
+        {
+            return $"Cliente({numeroCliente}) seu limite Total é de R${limiteTotalCliente} e no momento você possuí R${limiteCliente} de limite";
+        }
+
+        public void Sacar(int valor)
+        {
+            if (valor > saldoCliente)
+                return;
+
+            saldoCliente -= valor;
+        }
+
+        public void Depositar(int valor)
+        {
+            saldoCliente += valor;
+        }
+
+        public void Tranferir(int valor, string tipoDePagamento, ContaCorrente conta)
+        {
+            if (tipoDePagamento == "debito")
+            {
+                if (valor > saldoCliente)
+                    return;
+
+                saldoCliente -= valor;
+                conta.saldoCliente += valor;
+            }
+            else if (tipoDePagamento == "credito")
+            {
+                if (valor > limiteCliente)
+                    return;
+
+                limiteCliente -= valor;
+                conta.saldoCliente += valor;
+            }
+        }
+
+        public void PagarCartao(int valor)
+        {
+            if (valor > saldoCliente)
+                return;
+
+            if (valor > limiteTotalCliente - limiteCliente)
+                return;
+
+            limiteCliente += valor;
+            saldoCliente -= valor;
+        }
+    }
+}
---
+++ Atividade13_ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Atividade13_ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(5142, 1000, 1000, true);
+
+            ContaCorrente conta2 = new ContaCorrente(6874, 350, 1000, false);
+
+            conta1.Tranferir(200, "credito", conta2);
+
+            conta1.Depositar(50);
+
+            Console.WriteLine(conta1.VisualizarLimite());
+
+            Console.WriteLine(conta1.VisualizarSaldo());
+
+            conta1.PagarCartao(200);
+
+            Console.WriteLine(conta1.VisualizarLimite());
+
+            Console.WriteLine(conta1.VisualizarSaldo());
+
+            Console.WriteLine(conta2.VisualizarSaldo());
+
+            Console.WriteLine(conta2.VisualizarLimite());
+
+            Console.ReadLine();
+        }
+    }
+}
